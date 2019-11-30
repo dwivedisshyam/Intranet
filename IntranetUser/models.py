e2b15881a40937e0ad9ef7da132ad374cf96cc4f
dwivedisshyam <dwivedisshyam@gmail.com>
2019-11-30 19:02:14 +0530

    clear push

--- IntranetUser/models.py
+++ IntranetUser/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 # Create your models here.
 class Menu(models.Model):
-    text = models.TextField(max_length=20, blank=False)
+    text = models.CharField(max_length=20, blank=False)
     isDropdown = models.BooleanField(default=False)
-    url = models.TextField(blank=True)
+    url = models.CharField(max_length=40, blank=True)
 
     def __str__(self):
         return self.text
@@ -14,15 +14,15 @@ class Menu(models.Model):
 
 class SubMenu(models.Model):
     menuId = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    text = models.TextField(max_length=20, blank=False)
-    url = models.TextField(blank=False)
+    text = models.CharField(max_length=20, blank=False)
+    url = models.CharField(max_length=40, blank=False)
 
     def __str__(self):
         return self.text
 
 
 class Category(models.Model):
-    text = models.TextField(max_length=40, blank=False)
+    text = models.CharField(max_length=40, blank=False)
 
     def __str__(self):
         return self.text
@@ -30,7 +30,7 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     categoryId = models.ForeignKey(Category, on_delete=models.CASCADE)
-    text = models.TextField(max_length=40, blank=False)
+    text = models.CharField(max_length=40, blank=False)
     content = RichTextUploadingField(blank=False)
 
     def __str__(self):
